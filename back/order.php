2d40305a99c7ccebed776bdcaf97636786bf2f00
FY Kuo <teresa99339@gmail.com>
2022-09-05 20:26:27 +0800

    前台輪撥器完成

--- back/order.php
+++ back/order.php
@@ -1,1 +1,25 @@
-order
+<div class="grey0 p-10">
+    <div class="rb ct title">預告片清單</div>
+    <div class="my-10">
+        快速刪除：<input type="radio" name="fastDel" id="fastDel" value="date">依日期<input type="text" name="date" id="date">
+        <input type="radio" name="fastDel" id="fastDel" value="movie">依日期 <select name="movie" id="movie"></select>
+        <input type="button" value="刪除" onclick="fastDel()">
+    </div>
+    <div class="d-f">
+        <div class="w-15 grey1 ct" style="margin: 0 2px;">訂單編號</div>
+        <div class="w-15 grey1 ct" style="margin: 0 2px;">電影名稱</div>
+        <div class="w-15 grey1 ct" style="margin: 0 2px;">日期</div>
+        <div class="w-15 grey1 ct" style="margin: 0 2px;">場次時間</div>
+        <div class="w-15 grey1 ct" style="margin: 0 2px;">訂購數量</div>
+        <div class="w-15 grey1 ct" style="margin: 0 2px;">訂購位置</div>
+        <div class="w-15 grey1 ct" style="margin: 0 2px;">操作</div>
+    </div>
+    <div class="order_list over-aut my-10">
+
+    </div>
+</div>
+
+
+<script>
+    
+</script>
--- front/main.php
+++ front/main.php
@@ -1,11 +1,39 @@
 <div class="half" style="vertical-align:top;">
     <h1>預告片介紹</h1>
     <div class="rb tab" style="width:95%;">
-        <div id="abgne-block-20111227">
-            <ul class="lists">
-            </ul>
-            <ul class="controls">
-            </ul>
+        <div class="poster">
+            <div class="lists p-r d-f j-c" style="height: 70%;">
+                <?php
+                $posters = $Poster->all(['sh'=>1]," ORDER BY `rank` ");
+                foreach ($posters as $key => $poster) {
+                ?>
+                <div class="poster_item p-a d-f f-w j-c ct" data-ani="<?=$poster['ani']?>">
+                    <img src="./upload/<?=$poster['img']?>" alt="" style="height: 250px;">
+                    <div class="w-100">
+                        <?=$poster['name']?>
+                    </div>
+                </div>
+                <?php
+                }
+                ?>
+            </div>
+            <div class="controls d-f a-c" style="height: 30%;">
+                <div class="preBtn" onclick="move('left')"></div>
+                <div class="controls_item p-r d-f a-c">
+                    <div class="p-a controls_nav d-f a-c">
+                        <?php
+                        foreach ($posters as $key => $poster) {
+                        ?>
+                        <div class="img_item" style="height: 80px;" onclick="ani(<?=$key?>)">
+                            <img src="./upload/<?=$poster['img']?>" alt="" style="height: 80px;">
+                        </div>
+                        <?php
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="nextBtn" onclick="move('right')"></div>
+            </div>
         </div>
     </div>
 </div>
@@ -20,3 +48,91 @@
         <div class="ct"> </div>
     </div>
 </div>
+
+<script>
+    $('.poster_item').eq(0).show();
+
+    let setAni = setInterval(()=>{
+        ani()
+    },3000)
+
+    function ani(num){
+        let now = $('.poster_item:visible');
+        let next;
+        if(num == undefined){
+
+            if($(now).next().length == 0){
+                next = $('.poster_item').eq(0);
+            }else{
+                next = $(now).next();
+            }
+
+        }else{
+            next = $('.poster_item').eq(num);
+        }
+
+        let ani = $(next).data('ani');
+
+        // console.log(next);
+        // console.log(ani);
+
+        switch (ani) {
+            case 1:
+                $(now).fadeOut(2000,()=>{
+                    $(next).fadeIn(2000)
+                })
+            break;
+            case 2:
+                $(now).slideDown(2000,()=>{
+                    $(next).slideUp(2000)
+                })
+            break;
+            case 3:
+                $(now).hide(2000,()=>{
+                    $(next).show(2000)
+                })
+            break;
+        
+
+        }
+    }
+
+    let page = 0;
+
+    function move(type){
+        let nowLeft = $('.controls_nav').css('left').split('px')[0];
+        let move;
+        let num = <?=count($posters);?>;
+        let limit = 4;
+        let pages = parseInt(num)-parseInt(limit);
+
+        switch (type) {
+            case 'left':
+                if(page >= 0 && page < pages){
+                    page++
+                    move = parseInt(nowLeft)-86;
+                }
+            break;
+
+            case 'right':
+                if(page > 0 ){
+                    page--
+                    move = parseInt(nowLeft)+86;
+                }
+            break;
+    
+        }
+
+        // console.log(nowLeft);
+
+        $('.controls_nav').animate({left:move});
+    }
+
+    $('.img_item').hover(function(){
+        clearInterval(setAni);
+    },function(){
+        let setAni = setInterval(()=>{
+        ani()
+        },3000)
+    })
+</script>
